86a8bd0015360704a8b8d79a3edbd54560ee3e66
Hercules Silva <hercules.silva@dcx.ufpb.br>
2023-06-07 14:29:59 -0300

    style: added colors to theme

--- app/src/main/java/com/herco/jethelmetsstore/ui/theme/Theme.kt
+++ app/src/main/java/com/herco/jethelmetsstore/ui/theme/Theme.kt
@@ -10,31 +10,34 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFFF35959),
+    onPrimary = Color(0xFFFFFFFF),
+
+    surface = Color(0xFFFFFFFF),
+    onSurface = Color(0xFF2E2E2E),
+    onSurfaceVariant = Color(0xFF949494),
+
+    outline = Color(0xFFFAFAFA),
+    outlineVariant = Color(0xFF949494)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFFF35959),
+    onPrimary = Color(0xFFFFFFFF),
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    surface = Color(0xFFFFFFFF),
+    onSurface = Color(0xFF2E2E2E),
+    onSurfaceVariant = Color(0xFF949494),
+
+    outline = Color(0xFFFAFAFA),
+    outlineVariant = Color(0xFF949494)
 )
 
 @Composable
